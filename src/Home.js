107c3d64ad138094874fc7c31034dc2879fb7406
Ugne <ugne.adamo@gmail.com>
2023-07-09 17:11:28 +0200

    OLD STUFF

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,47 @@
 import Out from "./navbars/Out";
-import "./general.sass"
-import Scene from "./Scene";
+import "/Users/ugneadamo/todoblog/src/styling/Scene.scss"
+import * as THREE from 'three';
+import {useEffect} from 'react';
 
 function Home() {
-    return (  
-        <div className="mainwindow">
-            <Out/>
-            <Scene/>
-            Hello! this is where you'll see the initial page
-        </div>
+
+    useEffect( () => {
+      // building a red cube 
+    const scene = new THREE.Scene()
+    const geometry = new THREE.BoxGeometry(1,1,1) // units for side sizes
+    const material = new THREE.MeshBasicMaterial({color: 0xf8284})
+    
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
+  
+    // camera
+    const sizes = {
+      width: 800,
+      height: 600
+    }
+  
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)// desgrees of vision / width and height of vsion filed
+    camera.position.z = 4
+    camera.position.x = 0 // left AND right, can work with - ofc
+    camera.position.y = 1 // height
+    scene.add(camera)
+  
+    // rendered
+    const canvas = document.querySelector('canvas.backgr')
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas
+    })
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.render(scene,camera)
+  
+    
+    }, [])
+  
+    return (
+      <div className="holder" >
+        <Out/>
+        <canvas class="backgr"></canvas>
+      </div>
     );
 }
 
